92136662bbd667e9046aca74edeaac4d6d6e9054
Jacob <jacobgnewman@icloud.com>
2022-09-02 16:59:30 -0700

    Progress update

--- src/expr.rs
+++ src/expr.rs
@@ -1,5 +1,10 @@
-
+use std::rc::Rc;
+use crate::token::Token;
+use crate::rox_type::RoxType;
 
 pub enum Expr {
-
+    Binary(Rc<Expr>, Token, Rc<Expr>),
+    Literal(RoxType),
+    Grouping(Rc<Expr>),
+    Unary(Token,Rc<Expr>)
 }
